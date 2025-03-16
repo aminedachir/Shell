2b688d51c60a39e0b38e6b830e5fc627107bc6b8
aminedachir <dachiramine110@gmail.com>
2025-03-16 07:43:56 +0100

    update test

--- arrays_arguments_test.sh
+++ arrays_arguments_test.sh
@@ -20,23 +20,25 @@ aft_bay+=("Scientific Instruments")
 # Check if an argument is provided
 if [ $# -eq 0 ]; then
     echo "Please specify a cargo bay: forward, midship, or aft"
-    exit 1
 fi
 i=0
 # Display inventory based on the argument
 if [ "$1" = "forward" ]; then
+    echo "Forward Bay Inventory:"
     for i in {0..2};do
         echo "${forward_bay[i]}"
-    done 
+    done
 elif [ "$1" = "midship" ]; then
+    echo "Midship Bay Inventory:"
     for i in {0..2};do
         echo "${midship_bay[i]}"
-    done 
+    done
 elif [ "$1" = "aft" ]; then
+    echo "Aft Bay Inventory:"
     for i in {0..2};do
         echo "${aft_bay[i]}"
-    done 
+    done
 else
     echo "Invalid cargo bay. Choose forward, midship, or aft."
-    exit 1
 fi
+
